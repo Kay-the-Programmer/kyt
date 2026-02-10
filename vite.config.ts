318d6a66b2d0b41b0c938881b985c73f3def9909
Kanyanta Pythias <kanyantapythias072@gmail.com>
2026-02-10 15:17:48 +0200

    Initial loading Fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
@@ -16,7 +15,7 @@ export default defineConfig(({ mode }) => {
       port: 3000,
       host: '0.0.0.0',
     },
-    plugins: [react(), tailwindcss()],
+    plugins: [react()],
     define: {
       'process.env.VITE_GEMINI_API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY)
